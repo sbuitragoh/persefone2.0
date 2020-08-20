ebd3bf9afd99148d7ff6e595bb29eb861a4c09d9
sbuitragoh <sbuitragoh@unal.edu.co>
2020-08-20 09:23:44 -0500

    Re-ordering the file. Using the main() function in a proper way; also setting a new debug-print instance.

--- testPage.py
+++ testPage.py
@@ -8,8 +8,7 @@ from matplotlib import pyplot as plt
 from matplotlib import patches as pch
 import scipy as sc
 
-
-if __name__ == "__main__":
+def main():
     pagePath = './testImag/out_4.jpg'
 
     # PAGE ADQUISITION
@@ -17,27 +16,16 @@ if __name__ == "__main__":
     processed = pagpre.pageProcessing(pageOriginal, pageHSV, pageGrey)
     pageCut, limit = pagpre.limitExtract(processed)
     originalCut = pageOriginal[limit[1][0]:limit[1][1], limit[0][0]:limit[0][1]]
-    print ('Image Pre-processed!')
+    print('Image Pre-processed!')
 
     # LINE FINDING
 
     lineInterval, lines = lisol.lineSet(pageCut)
     lisol.lineCleaner(pageCut, lineInterval)
 
-    # WORD BOXES
+    # DEBUG STOP INSIDE MAIN
+    print('Inside-main debug stop.')
 
-    #Graph
-    # linePath = './lines/line_1.png'
-    # line = cv.imread(linePath)
-    # fig, ax = plt.subplots(1)
-    # ax.imshow(line)
-    #
-    # m, n = np.shape(wordBox)
-    # for pos in range(m):
-    #     x0 = wordBox[pos][1]
-    #     y0 = wordBox[pos][0]
-    #     dx = wordBox[pos][3] - wordBox[pos][1]
-    #     dy = wordBox[pos][2] - wordBox[pos][0]
-    #     rect = pch.Rectangle((x0, y0), dx, dy, facecolor='None', edgecolor='r', linewidth='1')
-    #     ax.add_patch(rect)
+if __name__ == "__main__":
+    main()
     print('Debug stop')
